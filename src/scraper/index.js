bd44f202d4694019ba0fcff47ee8e7b956125094
JacopoPatrocloMartinelli <jacopo.martinelli@farnedi.it>
2018-10-26 16:09:56 +0200

    rimosso screen pagina

--- src/scraper/index.js
+++ src/scraper/index.js
@@ -8,7 +8,6 @@ var page;
 module.exports = {
   scrapeImgByUsername: async (userName = 'patroclo94') => {
     await page.goto(`https://www.instagram.com/${userName}`);
-    await page.screenshot({ path: 'example.png' });
     const images = await page.$$eval('.FFVAD', els => els.map(el => el.src));
     return images;
   }
